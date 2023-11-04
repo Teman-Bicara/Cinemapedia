8cf55d5ef01d0c3bd936c176a17c7ca013eccfb4
kisnak21 <krisnastya21@gmail.com>
2023-11-04 15:36:29 +0700

    scaffolding project folder

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="w-screen h-screen mx-auto flex items-center justify-center">
+      <h1 className="text-3xl">Coming Soon!</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,5 @@
+const MovieList = () => {
+  return <div>movielist</div>;
+};
+
+export default MovieList;
